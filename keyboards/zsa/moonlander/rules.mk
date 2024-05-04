675e85a36f70756b0892878fddc85abe5cab2dfd
Robert Greener <dev@greener.sh>
2024-05-04 14:29:12 +0100

    Add moonlander keymap

--- keyboards/zsa/moonlander/rules.mk
+++ keyboards/zsa/moonlander/rules.mk
@@ -1,4 +1,21 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = yes        # Console for debug
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no          # Audio output
+# AUDIO_DRIVER = dac_additive
 CUSTOM_MATRIX = lite
+SWAP_HANDS_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
 
 VPATH += drivers/gpio
 SRC += matrix.c mcp23018.c
