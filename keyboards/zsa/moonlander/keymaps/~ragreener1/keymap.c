80796ee82e7f3d855f6dfcc929e9dfd0b5aba02c
Robert Greener <dev@greener.sh>
2024-05-04 19:00:06 +0100

    Add RGB layers

--- keyboards/zsa/moonlander/keymaps/~ragreener1/keymap.c
+++ keyboards/zsa/moonlander/keymaps/~ragreener1/keymap.c
@@ -1,4 +1,5 @@
-/* Copyright 2020 ZSA Technology Labs, Inc <@zsa>
+/* Copyright 2024 Robert Greener <dev at greener dot sh>
+ * Copyright 2020 ZSA Technology Labs, Inc <@zsa>
  * Copyright 2020 Jack Humbert <jack.humb@gmail.com>
  * Copyright 2020 Christopher Courtney, aka Drashna Jael're  (@drashna) <drashna@live.com>
  *
@@ -73,3 +74,125 @@ const uint16_t PROGMEM combo0[] = { US_4, US_5, US_6, US_7, COMBO_END};
 combo_t key_combos[] = {
     COMBO(combo0, TG(QWERTY)),
 };
+
+
+extern rgb_config_t rgb_matrix_config;
+
+void keyboard_post_init_user(void) {
+  rgb_matrix_enable();
+}
+
+
+#define RGB_RDC {255, 0, 0}
+#define RGB_IND {0x61, 0, 0xFF}
+#define RGB_BRN {0, 240, 255}
+#define RGB_WTR {0, 14, 255}
+#define RGB_NGT {0, 5, 84}
+#define RGB_YLW {0xFF, 0xF5, 0}
+#define RGB_BLK {0, 0, 0}
+#define RGB_SPK {0, 0x75, 0xFF}
+#define RGB_FFA {0xFF, 0x00, 0x8A}
+
+const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
+    [SYMB] = {
+        // left col 1
+        RGB_RDC, RGB_IND, RGB_NGT, RGB_NGT, RGB_BLK,
+        // left col 2
+        RGB_RDC, RGB_IND, RGB_IND, RGB_NGT, RGB_BLK,
+        // left col 3
+        RGB_RDC, RGB_IND, RGB_IND, RGB_YLW, RGB_BLK,
+        // left col 4
+        RGB_RDC, RGB_IND, RGB_IND, RGB_YLW, RGB_BLK,
+        // left col 5
+        RGB_RDC, RGB_IND, RGB_YLW, RGB_YLW, RGB_BLK,
+        // left col 6
+        RGB_RDC, RGB_IND, RGB_YLW, RGB_YLW,
+        // left col 7
+        RGB_BLK, RGB_BLK, RGB_BLK,
+        // left thumb
+        RGB_RDC, RGB_NGT, RGB_BLK, RGB_BLK,
+        // right col 7
+        RGB_RDC, RGB_RDC, RGB_NGT, RGB_WTR, RGB_BLK,
+        // right col 6
+        RGB_RDC, RGB_WTR, RGB_WTR, RGB_WTR, RGB_BLK,
+        // right col 5
+        RGB_RDC, RGB_WTR, RGB_WTR, RGB_WTR, RGB_BLK,
+        // right col 4
+        RGB_RDC, RGB_BRN, RGB_BRN, RGB_BRN, RGB_BLK,
+        // right col 3
+        RGB_RDC, RGB_BRN, RGB_BRN, RGB_BRN, RGB_BLK,
+        // right col 2
+        RGB_RDC, RGB_BRN, RGB_BRN, RGB_BRN,
+        // right col 1
+        RGB_BLK, RGB_BLK, RGB_BLK,
+        // right thumb
+        RGB_BRN, RGB_NGT, RGB_BLK, RGB_BLK
+    },
+    [SYS] = {
+        // left col 1
+        RGB_YLW, RGB_BLK, RGB_BLK, RGB_BLK, RGB_BLK,
+        // left col 2
+        RGB_YLW, RGB_BLK, RGB_BRN, RGB_BLK, RGB_BLK,
+        // left col 3
+        RGB_YLW, RGB_SPK, RGB_BRN, RGB_BLK, RGB_BLK,
+        // left col 4
+        RGB_YLW, RGB_SPK, RGB_BRN, RGB_BLK, RGB_BLK,
+        // left col 5
+        RGB_YLW, RGB_SPK, RGB_BRN, RGB_BLK, RGB_BLK,
+        // left col 6
+        RGB_YLW, RGB_BLK, RGB_BLK, RGB_BLK,
+        // left col 7
+        RGB_BLK, RGB_BLK, RGB_BLK,
+        // left thumb
+        RGB_BLK, RGB_BLK, RGB_BLK, RGB_BLK,
+        // right col 7
+        RGB_RDC, RGB_BLK, RGB_BLK, RGB_BLK, RGB_BLK,
+        // right col 6
+        RGB_BLK, RGB_BLK, RGB_BLK, RGB_BLK, RGB_BLK,
+        // right col 5
+        RGB_BLK, RGB_YLW, RGB_SPK, RGB_BLK, RGB_BLK,
+        // right col 4
+        RGB_BLK, RGB_YLW, RGB_SPK, RGB_FFA, RGB_BLK,
+        // right col 3
+        RGB_BLK, RGB_YLW, RGB_SPK, RGB_FFA, RGB_BLK,
+        // right col 2
+        RGB_BLK, RGB_YLW, RGB_YLW, RGB_BLK,
+        // right col 1
+        RGB_BLK, RGB_BLK, RGB_BLK,
+        // right thumb
+        RGB_RDC, RGB_BLK, RGB_BLK, RGB_BLK
+    }
+};
+
+void set_layer_color(int layer) {
+  for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+    RGB rgb = {
+      .r = pgm_read_byte(&ledmap[layer][i][0]),
+      .g = pgm_read_byte(&ledmap[layer][i][1]),
+      .b = pgm_read_byte(&ledmap[layer][i][2]),
+    };
+    if (!rgb.r && !rgb.g && !rgb.b) {
+        rgb_matrix_set_color( i, 0, 0, 0 );
+    } else {
+        float f = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+        rgb_matrix_set_color( i, f * rgb.r, f * rgb.g, f * rgb.b );
+    }
+  }
+}
+
+bool rgb_matrix_indicators_user(void) {
+  if (keyboard_config.disable_layer_led) { return false; }
+  switch (biton32(layer_state)) {
+    case SYMB:
+      set_layer_color(SYMB);
+      break;
+    case SYS:
+      set_layer_color(SYS);
+      break;
+   default:
+    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
+      rgb_matrix_set_color_all(0, 0, 0);
+    break;
+  }
+  return true;
+}
