54ed2b7a8bd9a00cb0bbb07201074672b57c8af1
Robert Greener <dev@greener.sh>
2024-05-12 09:31:30 +0100

    Set windows layer to be colemak-dh

--- keyboards/keychron/q10_max/iso_encoder/keymaps/~ragreener1/keymap.c
+++ keyboards/keychron/q10_max/iso_encoder/keymaps/~ragreener1/keymap.c
@@ -21,7 +21,7 @@
 enum layers {
     MAC_BASE,
     MAC_FN,
-    WIN_BASE,
+    WIN_BASE_DH,
     WIN_FN,
 };
 
@@ -43,12 +43,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    _______,  _______,  _______,  _______,  _______,  _______,   BAT_LVL,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,    _______,  _______,            _______,  _______,  _______,             _______,            _______,            _______,            _______,  _______,  _______),
 
-    [WIN_BASE] = LAYOUT_91_iso(
+    [WIN_BASE_DH] = LAYOUT_91_iso(
         KC_MUTE,    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,             KC_DEL,
-        MC_1,       KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
-        MC_2,       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,                      KC_PGDN,
-        MC_3,       KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,      KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,             KC_HOME,
-        MC_4,       KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
+        MC_1,       UK_GRV,   UK_1,     UK_2,     UK_3,     UK_4,     UK_5,      UK_6,     UK_7,     UK_8,     UK_9,     UK_0,     UK_MINS,  UK_EQL,   KC_BSPC,            KC_PGUP,
+        MC_2,       KC_TAB,   UK_Q,     UK_W,     UK_F,     UK_P,     UK_B,      UK_J,     UK_L,     UK_U,     UK_Y,     UK_SCLN,  UK_LBRC,  UK_RBRC,                      KC_PGDN,
+        MC_3,       KC_CAPS,  UK_A,     UK_R,     UK_S,     UK_T,     UK_G,      UK_M,     UK_N,     UK_E,     UK_I,     UK_O,     UK_QUOT,  UK_HASH,  KC_ENT,             KC_HOME,
+        MC_4,       KC_LSFT,  UK_Z,     UK_X,     UK_C,     UK_D,     UK_V,      UK_BSLS,  UK_BSLS,  UK_K,     UK_H,     UK_COMM,  UK_DOT,   UK_SLSH,  KC_RSFT,  KC_UP,
         MC_5,       KC_LCTL,  KC_LWIN,            KC_LALT,  KC_SPC,  MO(WIN_FN),           KC_SPC,             KC_RALT,            KC_RCTL,            KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [WIN_FN] = LAYOUT_91_iso(
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [MAC_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [MAC_FN] = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)},
-    [WIN_BASE] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [WIN_BASE_DH] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
     [WIN_FN] = {ENCODER_CCW_CW(RGB_VAD, RGB_VAI)}
 };
 #endif // ENCODER_MAP_ENABLE
